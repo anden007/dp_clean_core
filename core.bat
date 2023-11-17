dbcaa45fbc146fe4c87d671b579cb90e549808c6
周毅 <3446799@qq.com>
2023-11-17 10:12:18 +0800

    新增模板更新助手

---
+++ core.bat
@@ -0,0 +1,26 @@
+@echo off
+chcp 65001 > nul
+set command=%1
+
+if "%command%" == "" (
+	goto Help
+) else if "%command%" == "tpl" (
+	goto Gen_Tpl
+) else (
+	goto Help
+)
+
+:Gen_Tpl
+@echo ◈ 更新VUE模板中...
+hero -source="graph/extgen/template" -extensions=".tpl"
+hero -source="template" -extensions=".tpl"
+@echo ✓ 数据模型CRUD、VUE生成模板更新完成
+goto End
+
+:Help
+@echo 参数错误,命令正确用法如下：
+@echo core [command]
+@echo   - command:
+@echo     - tpl 更新数据模型CRUD、VUE生成模板文件,一般不常用 
+:End
+
