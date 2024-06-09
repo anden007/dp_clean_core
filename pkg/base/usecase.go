ed9c952236c593ab60d9070e972cad2aeb1bb537
anden007 <3446799@qq.com>
2024-06-09 12:57:16 +0800

    扩展只读模块接口，降低系统安全风险

--- pkg/base/usecase.go
+++ pkg/base/usecase.go
@@ -7,14 +7,18 @@ import (
 )
 
 type ICRUDUsecase[T IDBModel] interface {
+	IReadOnlyUsecase[T]
 	Add(entity T, ctx context.Context, transId string) (err error)
 	DelByIds(ids []string, ctx context.Context, transId string) (err error)
 	Edit(entity T, ctx context.Context, transId string) (err error)
 	Updates(id string, fieldValues map[string]interface{}, ctx context.Context, transId string) (err error)
+	Commit(transId string) (err error)
+	Rollback(transId string) (err error)
+}
+
+type IReadOnlyUsecase[T IDBModel] interface {
 	GetAll() (result []T, err error)
 	GetById(id string) (result T, err error)
 	GetByIds(ids []string) (result []T, err error)
 	GetByCondition(condition map[string]pkg.SearchCondition) (result []T, total int64, err error)
-	Commit(transId string) (err error)
-	Rollback(transId string) (err error)
 }
